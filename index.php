7c133d8d41cdcdb4ddad8199e5a7f7818a4ec5c3
guganems <g_nemsveridze@cu.edu.ge>
2019-03-20 17:21:55 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guga
+ * Date: 20.03.2019
+ * Time: 16:57
+ */
+?>
+
+<html>
+    <form method="get" action="process.php">
+        <input type="text" name="word"/>
+        <input type="submit" name="submit"/>
+    </form>
+</html>
---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guga
+ * Date: 20.03.2019
+ * Time: 17:19
+ */
+
+$word = $_GET["word"];
+
